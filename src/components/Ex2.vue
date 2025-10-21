8d9339022731336a3b4838cb46d640de2b1b2619
leeshel <l.weasley.2024@computing.smu.edu.sg>
2025-10-21 06:04:09 +0000

    updated ex2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,12 +1,23 @@
 <script>
+import { getEnabledCategories } from 'trace_events';
+
     export default { 
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                itemAdded: ''
             }
             
+        },
+
+        methods:{
+            addItem(){
+                this.items.push(this.itemAdded);
+                this.itemAdded = ''
+            }
         }
+
     }
 </script>
 
@@ -14,5 +25,15 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+     <ul>
+        <li v-for="(item, index) in items" :key="index"> 
+            {{ item }}
+            <button v-on:click="items.splice(index, 1)">Delete!</button>
+        </li>
+     </ul>
+
+     <!--@click is the shorthand-->
+     <input type="text" v-model="itemAdded"> </input>
+     <button v-on:click="addItem()" >Add!</button>
    
 </template>
