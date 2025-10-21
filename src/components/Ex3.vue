71c2c78e79ff749aec41592ed3de6465f8726ff2
leeshel <l.weasley.2024@computing.smu.edu.sg>
2025-10-21 07:03:58 +0000

    updated ex3

--- src/components/Ex3.vue
+++ src/components/Ex3.vue
@@ -1,8 +1,51 @@
 <script>
+import axios from 'axios';
+
     export default { 
-
        // add code here
+       data(){
+        return{
+            moods:["happy", "sad", "angry"],
+            subject: '',
+            entry: '',
+            mood: '',
+            statusMessage: ''
+        }
+       },
+       
+       computed:{
+        baseUrl(){
+            if (window.location.hostname=='localhost')
+                return 'http://localhost:3000'
+            else{
+                const codespace_host = window.location.hostname.replace('5173','3000')
+                return `https://${codespace_host}`; //backticks here
+            }
+        } 
+       },
 
+       methods:{
+        addPost(){
+            axios.get(`${this.baseUrl}/addPost`,{ //not single quotation mark! (') its backtick: (`))
+                params: {
+                    subject: this.subject,
+                    entry: this.entry,
+                    mood: this.mood
+
+                }
+            })
+            .then(response=>{
+                this.statusMessage = "Post added successfully";
+                //clear fields
+                this.subject= '';
+                this.entry= '';
+                this.mood= '';
+            }
+            ).catch(error=>{
+                this.statusMessage = "Try again!";
+            });
+         }
+        }
     }
 </script>
 
@@ -20,10 +63,17 @@
         Mood:
         <!-- TODO: Build a dropdown list here for selecting the mood -->
 
+        <!--required specifies a user must fill in a value before submitting form-->
+        <select v-model="mood" required>
+            <option v-for="mood in moods"> {{ mood }}</option>
+        </select>
+
         <br>
 
         <br>
-        <button>Submit New Post</button>
+        <button @click="addPost()">Submit New Post</button>
+
+        <p>{{ statusMessage }}</p>
 
         <hr> Click  <a><router-link to="/ViewPosts/">here</router-link></a>  to return to Main Page
        
