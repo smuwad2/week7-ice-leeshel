98a975089620b2077bac8938e59be63c974d4bc4
leeshel <l.weasley.2024@computing.smu.edu.sg>
2025-10-24 13:48:16 +0000

    updated ex3.spec.ts

--- e2e/ex3.spec.ts
+++ e2e/ex3.spec.ts
@@ -6,7 +6,8 @@ test('Ex3 add blog post test', async ({mount}) => {
     const component = await mount(Ex3);
     await component.getByRole('textbox').first().fill('New Blog Post');
     await component.locator('textarea').fill('This is the content of the new blog post.');
-    await component.getByRole('combobox').selectOption('Angry');
+    await component.locator('select').selectOption({ label: 'angry' });
+    // await component.getByRole('combobox').selectOption('Angry');
     await component.getByRole('button', {name: 'Submit New Post'}).click();
     
     const newResponse = await axios.get('http://localhost:3000/posts');
@@ -14,7 +15,8 @@ test('Ex3 add blog post test', async ({mount}) => {
     const addedPost = newPosts[0]
     expect(addedPost.subject).toBe('New Blog Post');
     expect(addedPost.entry).toBe('This is the content of the new blog post.');
-    expect(addedPost.mood).toBe('Angry');
+    //expect(addedPost.mood).toBe('Angry');
+    expect(addedPost.mood).toBe('angry');
 
     await axios.get('http://localhost:3000/deletePost?id=' + addedPost.id);
 });
